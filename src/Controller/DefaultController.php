c55a71c99100ead123c9670fa6d62ae3a2dcdfed
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-06-27 21:40:59 +0200

    quest 26: Internationalisation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,5 +23,15 @@ class DefaultController extends AbstractController
         return $this->render('layout/navbartop.html.twig', [
             'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
         ]);
+    }
+
+    /**
+     * @Route("/change_locale", name="change_locale")
+     */
+    public function language(Request $request, string $_locale): Response
+    {
+        $request->setLocale($_locale);        
+
+        return $this->redirectToRoute('app_index');
     }
 }
